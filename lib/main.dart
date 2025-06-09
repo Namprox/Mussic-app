2a4af1c2aaac086aa36543145898b7ef9695c6de
Namprox <namky1602@gmail.com>
2025-06-09 21:14:48 +0700

    init project

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:demo/models/playlist_provider.dart';
+import 'package:demo/themes/theme_prodiver.dart';
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart';
+import 'package:provider/provider.dart';
+
+void main() {
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (context) => ThemeProvider()),
+        ChangeNotifierProvider(create: (context) => PlaylistProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+      theme: Provider.of<ThemeProvider>(context).themeData,
+    );
+  }
+}
